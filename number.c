d5228f093f1878774915658ccca747b07c940d62
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-02-08 10:29:06 +0000

    Create number.c

---
+++ number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+int a,i,n;
+printf("Enter the number:\n");
+scanf("%d",&a);
+for(i=1;i<=n;i++)
+{
+    n=n/10;
+    printf("Print the all letter:",n);
+}
+
+    return 0;
+}
