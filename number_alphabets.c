982987ce89ddf41f104bf8eb6bb60ec556e80166
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-02-09 14:25:26 +0530

    Create number_alphabets.c

---
+++ number_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int numbers,a;
+    char alphabets;
+    printf("Enter the alphabets:");
+    scanf("%c",&alphabets);
+    printf("Enter the numbers:");
+    scanf("%d",&numbers);
+    a="numbers","alphabets";
+    if(a="alphabets","numbers")
+    {
+        printf("yes");
+    }
+    else
+{
+    printf("No");
+}
+}
