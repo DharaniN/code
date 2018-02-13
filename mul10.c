e2de1afae5df40dcd30061f60abf50670176b410
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-02-13 16:55:08 +0530

    Create mul10.c

---
+++ mul10.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int number,n;
+    printf("enter the number");
+    scanf("%d",&num);
+    while(number%10!=0)
+    {
+        number++;
+    }
+printf("earest greater multiple of 10 %d",number);
+return 0;
+    }
