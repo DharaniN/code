07d4bb4e4ed848c8d34c70be8ccc1ae298c80d1c
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-02-13 16:57:57 +0530

    Create mul 7.c

---
+++ mul 7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("Enter teh value");
+  scanf("%d",&n);
+  if(n%7==0)
+  {
+  printf("\n Multiple of 7");
+  }
+  else
+  {
+  printf("\n Not Multiple of 7");  
+  }return 0;
+}
