96d4150224a2c89bd212d3c900e948a5048f17e8
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-02-22 16:24:24 +0530

    Create NLR.c

---
+++ NLR.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+   int L,N,R;
+   printf("Enter the three value");
+   scanf("%d%d%d",&L,&N,&R);
+   if(n>l&&n<r)
+   {
+   	printf("Yes");
+   }
+   else
+   {
+   printf("No");
+   }
+}
