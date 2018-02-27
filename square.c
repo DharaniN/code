3f9053911c501d62e14ac5cb1634d9bd2988383b
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-02-27 16:10:05 +0530

    Create square.c

---
+++ square.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,i;
+printf("Enter the number:");
+scanf("%d%d",&n1,&n2);
+p=n1*n2;
+for(i=0;i<(p/2);i++)
+{
+if((p(i,2)==p)
+{
+printf("product is perfect square");
+break;
+}
+}
+}
