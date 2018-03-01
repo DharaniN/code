238eec91987d558580b27dd6d395c3d456f76905
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-03-01 16:03:28 +0530

    Create new file

---
+++ dddddddd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main () 
+{
+int m,n,mod, div;
+printf("Enter the value of m:");
+scanf("%d", &a);
+printf("Enter the value of n:");
+scanf("%d",&n);
+mod=m%n;
+div=m/n;
+printf ("%d",mod);
+printf("%d",div);
+return 0;
+}
